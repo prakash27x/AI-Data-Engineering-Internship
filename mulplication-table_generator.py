6ac7366dba70ed948c940689c71f68e623afddd5
Prakash Mahara <64721258+prakash27x@users.noreply.github.com>
2026-04-17 20:17:10 +0545

    rm

--- mulplication-table_generator.py
+++
@@ -1,7 +0,0 @@
-print("Welcome to Multiplication Table Generator")
-number = int(input("Enter any number: "))
-
-multiplier = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-for i in multiplier:
-    print(f"{number} * {i} = {number*i}")
-print("The End")
